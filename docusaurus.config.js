1dd51880b65029db82661179067f4a97b38eb393
Deep Patel <deeppatel234@gmail.com>
2024-06-30 12:33:05 +0530

    rename folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          path: './messaging',
+          routeBasePath: 'messaging',
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
