f1c3cfb90278464f292e5fd8636d9c8013f743d0
Deep Patel <deeppatel234@gmail.com>
2024-07-03 21:34:14 +0530

    add grag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,91 +4,98 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'EXT Browser',
-  tagline: 'Browser Extension Development Made Easy',
-  favicon: 'img/favicon.svg',
+  title: "EXT Browser",
+  tagline: "Browser Extension Development Made Easy",
+  favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: 'https://ext-browser.github.io/',
+  url: "https://ext-browser.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ext-browser', // Usually your GitHub org/user name.
-  projectName: 'ext-browser.github.io', // Usually your repo name.
+  organizationName: "ext-browser", // Usually your GitHub org/user name.
+  projectName: "ext-browser.github.io", // Usually your repo name.
   deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          path: './messaging',
-          routeBasePath: 'messaging',
+          sidebarPath: "./sidebars.js",
+          path: "./messaging",
+          routeBasePath: "messaging",
           remarkPlugins: [
-            [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
           ],
           editUrl:
-            'https://github.com/ext-browser/ext-browser.github.io/tree/main/',
+            "https://github.com/ext-browser/ext-browser.github.io/tree/main/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
+  ],
+
+  plugins: [
+    "@docusaurus/plugin-google-gtag",
+    {
+      trackingID: "G-C7RP0SNMB7",
+    },
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         respectPrefersColorScheme: false,
       },
       navbar: {
-        title: 'EXT Browser',
+        title: "EXT Browser",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'messagingSidebar',
-            position: 'left',
-            label: 'Messaging',
+            type: "docSidebar",
+            sidebarId: "messagingSidebar",
+            position: "left",
+            label: "Messaging",
           },
           {
-            href: 'https://github.com/ext-browser',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/ext-browser",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} EXT Browser.`,
       },
       prism: {
